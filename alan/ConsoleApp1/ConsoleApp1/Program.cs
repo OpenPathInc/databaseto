1c1ede12bd28860ac44183596daeec20ead511c6
tottiliyt <58280109+tottiliyt@users.noreply.github.com>
2021-01-16 15:17:24 +0800

    test commit

---
+++ alan/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using ClassLibrary1;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            var account = new BankAccount("alan", 100);
+            Console.WriteLine($"Account {account.Number} is created for {account.Owner} with {account.Balance}");
+        }
+    }
+}
